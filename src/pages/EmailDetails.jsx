2d883035672725c41a16b66eda8825eb52f4ebdd
Eugene <jeneea45@gmail.com>
2024-06-01 14:32:51 +0300

    fix compose (works also in email details now)

--- src/pages/EmailDetails.jsx
+++ src/pages/EmailDetails.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 import { mailService } from '../services/mail.service'
 import { HeaderDetails } from '../cmps/HeaderDetails'
+import { Compose } from '../cmps/Compose'
 
 export function EmailDetails() {
   const [mail, setMail] = useState()
   const params = useParams()
+  const [searchParams,setSearchParams] =useSearchParams()
 
   useEffect(() => {
     getMail()
   }, [params.mailId])
+  async function onGetNewNessage(newMessage) {
+    await mailService.save(newMessage)
+    updateFilter()
+  }
+
 
   async function getMail() {
     const data = await mailService.getById(params.mailId)
@@ -20,6 +27,9 @@ export function EmailDetails() {
 
   return (
     <div className="email-details">
+      {searchParams.get('compose') === 'true' && (
+            <Compose onGetNewNessage={onGetNewNessage} />
+          )}
       <HeaderDetails mail={mail} />
       {mail && (
         <div className="details-main">
